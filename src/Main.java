5dd30d8c30ebf3e7b1a04fc763aa2b3057f2796a
Dmitry5050 <gmail50@inbox.ru>
2025-03-28 19:32:41 +0300

    one more spase

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         // The first error is in this block
         System.out.println("***** Welcome to simple program *****");
         System.out.println("*\tThis program contains 3 errors\t*");
-
         System.out.println("Please enter a natural number between 1 and 5:");
         // The second error is in this block
         Scanner scanner = new Scanner(System.in);
@@ -14,8 +13,10 @@ public class Main {
 
         if (naturalNumber < 1 || naturalNumber > 5) {
             System.out.println("Your  number is not in the specified range!");
+
+
         } else if (naturalNumber == 1) {
-            System.out.println("Hi, how   are you doing?");
+            System.out.println("Hi, how   are  you doing?");
         } else if (naturalNumber == 2) {
             // The third error is in this block
             System.out.println("Have a good day!");
